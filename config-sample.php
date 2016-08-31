266a256aada09b99fa256dc230ab25b2ea1972ae
Joel Smith <erunaheru@gmail.com>
2016-08-31 11:52:29 -0400

    better support for realative dates from feeds, rate limit scraping with mysql backend for caching feeds, moved config to external file

---
+++ config-sample.php
@@ -0,0 +1,7 @@
+<?php
+define('mysql_hostname','localhost');
+define('mysql_user','xpath');
+define('mysql_password','xpath');
+define('mysql_database','xpath');
+
+define('inifolder','/var/www/rssconf/');
